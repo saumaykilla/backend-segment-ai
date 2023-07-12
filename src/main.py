3f485cd185d51f5766969e5662d15bb57b6327d3
Abhinav KM <abhinav.m@zerone-consulting.com>
2023-07-12 13:53:11 +0530

    Convert Form data to pydantic model

--- src/main.py
+++ src/main.py
@@ -2,6 +2,36 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import ORJSONResponse
 from pydantic import BaseModel
+from fastapi.encoders import jsonable_encoder
+from pydantic.fields import ModelField
+from typing import Type
+import inspect
+from fastapi import Form
+
+
+def as_form(cls: Type[BaseModel]):
+    new_parameters = []
+
+    for field_name, model_field in cls.__fields__.items():
+        model_field: ModelField  # type: ignore
+
+        new_parameters.append(
+             inspect.Parameter(
+                 model_field.alias,
+                 inspect.Parameter.POSITIONAL_ONLY,
+                 default=Form(...) if model_field.required else Form(model_field.default),
+                 annotation=model_field.outer_type_,
+             )
+         )
+
+    async def as_form_func(**data):
+        return cls(**data)
+
+    sig = inspect.signature(as_form_func)
+    sig = sig.replace(parameters=new_parameters)
+    as_form_func.__signature__ = sig  # type: ignore
+    setattr(cls, 'as_form', as_form_func)
+    return cls
 
 
 app = FastAPI()
@@ -16,37 +46,41 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+@as_form
 class Employee(BaseModel):
     employeeId : int
     firstName : str
     firstName : str
     lastName : str
+    employeePhone: str
     employeeEmail : str
 
 
 employee_data = [
     {
-        "employeeId": "1",
+        "employeeId": 1,
         "firstName": "Richard",
         "lastName": "Hendricks",
         "employeePhone": "(158) 389-2794",
         "employeeEmail": "richard@piedpiper.com",
     },
     {
-        "employeeId": "2",
+        "employeeId": 2,
         "firstName": "Jared",
         "lastName": "Dunn",
         "employeePhone": "(518) 390-2749",
         "employeeEmail": "jared@piedpiper.com",
     },
     {
-        "employeeId": "3",
+        "employeeId": 3,
         "firstName": "Erlich",
         "lastName": "Bachman",
         "employeePhone": "(815) 391-2974",
         "employeeEmail": "erlich.bachman@piedpiper.com",
     },
 ]
+
+employee_data = [Employee(**item) for item in employee_data]
 
 
 @app.get("/")
@@ -61,9 +95,10 @@ async def hello(name: str):
 
 @app.get("/employees")
 async def list_employees():
-    return ORJSONResponse(employee_data)
+    return ORJSONResponse(jsonable_encoder(employee_data))
 
 @app.post("/add_employee")
 async def add_employee(employee: Employee):
-    print(employee.__dict__)
+    employee_data.append(employee)
+
     return 200
